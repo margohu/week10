b6a66d8779a0a71ed6578b84944d31124e46ae37
margohu <marxhytt@gmail.com>
2022-09-08 15:06:51 +0300

    Ex56 and 65, week3

---
+++ week3/Ex65.java
@@ -0,0 +1,27 @@
+package week3;
+
+import java.util.ArrayList;
+
+public class Ex65 {
+
+    public static ArrayList<Integer> lengths(ArrayList<String> list) {
+        ArrayList<Integer> textLengths = new ArrayList<Integer>();
+        for (String text :list) {
+            textLengths.add(text.length());
+        }
+        return textLengths;
+        // write your code here
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Hallo");
+        list.add("Moi");
+        list.add("Benvenuto!");
+        list.add("badger badger badger badger");
+        ArrayList<Integer> lengths = lengths(list);
+
+        System.out.println("The lengths of the Strings: " + lengths);
+    }
+
+}
