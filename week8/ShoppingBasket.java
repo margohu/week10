f6b73ec47decd0f3777331f0dbec58a6f1934d63
margohu <marxhytt@gmail.com>
2022-09-22 08:52:54 +0300

    Week8, ex 12.1 Shop

---
+++ week8/ShoppingBasket.java
@@ -0,0 +1,30 @@
+package week8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingBasket {
+    private List<Purchase> purchases;
+
+    public ShoppingBasket() {
+        purchases = new ArrayList<Purchase>();
+    }
+
+    public void add(String product, int price){
+        this.purchases.add(new Purchase(product, 1, price));
+    }
+
+    public int price(){
+        int price = 0;
+        for (Purchase purchase: this.purchases) {
+            price += purchase.price();
+        }
+        return price;
+    }
+
+    public void print(){
+        for (Purchase purchase: this.purchases) {
+            System.out.println(purchase);
+        }
+    }
+}
