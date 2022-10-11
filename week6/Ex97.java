cb746dc2948aff08267858b62d550b3a81295d36
margohu <marxhytt@gmail.com>
2022-10-11 12:45:15 +0300

    Week6: ex97- elegant printing of an array

---
+++ week6/Ex97.java
@@ -0,0 +1,15 @@
+package week6;
+
+public class Ex97 {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+    public static void printElegantly(int[] array) {
+        for (int number:array) {
+            System.out.println(number + ',');
+        }
+    }
+
+}
