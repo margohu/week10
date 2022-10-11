4437397c2c977fd7c9feafe5192b23443dcad65b
margohu <marxhytt@gmail.com>
2022-10-11 10:33:11 +0300

    Week4: ex70- combiningArraylists

--- week4/Ex70.java
+++ week4/Ex70.java
@@ -13,20 +13,16 @@ public class Ex70 {
         ArrayList<Integer> list2 = new ArrayList<Integer>();
 
         Collections.addAll(list1, 4, 3);
-
         list1.add(4);
         list1.add(3);
 
         Collections.addAll(list2, 5, 10, 7);
-
         list2.add(5);
         list2.add(10);
         list2.add(7);
 
         combine(list1, list2);
-
         System.out.println(list1);
-
         System.out.println(list2);
     }
 }
