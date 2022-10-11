af912c603d623184aa972435b0aad071006e8e25
margohu <marxhytt@gmail.com>
2022-10-11 09:26:11 +0300

    Week1: ex7-multiplication

---
+++ week/Ex7.java
@@ -0,0 +1,11 @@
+package week;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Integer first = 277;
+        Integer second = 111;
+        Integer multiplication = first * second;
+        Integer square = multiplication * multiplication;
+        System.out.println("Korrutame: " + (first) + " * " + (second) + " = " + (multiplication) +'\n' + "See on ruudus: " + (square));
+    }
+}
