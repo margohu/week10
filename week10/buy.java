6efd1e60b49f52d1a22ead1fe20903b3188f3797
margohu <marxhytt@gmail.com>
2022-10-14 10:06:37 +0300

    Week10- buy

---
+++ week10/buy.java
@@ -0,0 +1,25 @@
+package week10;
+
+public class buy {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+
+        DeluxBurger db = new DeluxBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        System.out.println("Total Deluxe Burger price is " + db.itemizeHamburger());
+
+
+        HealthyBurger hb = new HealthyBurger("Bacon", 5.67);
+        hb.addHamburgerAddition1("Egg", 5.43);
+        hb.addHealthyAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is  " + hb.itemizeHamburger());
+
+
+    }
+
+}
